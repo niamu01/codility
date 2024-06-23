c0b81b99b1d92634ee4d6cc2f3e0c3abf3f725f0
niamu01 <niamu01@naver.com>
2024-06-23 19:19:54 +0900

    fix Lesson 5 Prefix Sums-CountDiv

--- Lesson 5 Prefix Sums/CountDiv/solution.cpp
+++ Lesson 5 Prefix Sums/CountDiv/solution.cpp
@@ -5,9 +5,9 @@
 // cout << "this is a debug message" << endl;
 
 int solution(int A, int B, int K) {
-  int first = ((A % K) == 0) ? A : (A / K + 1) * K;
+  int first = ((A % K) == 0) ? A : (A / K + 1);
 
   if (first > B) return 0;
 
-  return (B / K) - ((A - 1) / K);
+  return (B / K) - ((A > 0) ? (A - 1) / K : -1);
 }
